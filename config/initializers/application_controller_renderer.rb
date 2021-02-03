92cfb7ff9e9c13230001a5f80b8ba49b2f3422b2
DaikiKujirai <kuuuuuuujira@gmail.com>
2021-02-03 16:06:45 +0000

    comment

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '5a4918e1e76e30379ed90ccb0f99b908736ba3d02a8442075e6d12986dc82c6267fb204949290a088daef5b1b00d1bafa30ebef3e9bb25a8d015f238b0e55942'
